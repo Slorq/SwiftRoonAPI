dc13875459d301b3171624009aa0caff82da71d0
Slorq <alejandro_maya_a@hotmail.com>
2023-07-02 12:38:24 +0200

    Use shared resource

--- Tests/SwiftRoonAPITests/Moo/MooEncoderTests.swift
+++ Tests/SwiftRoonAPITests/Moo/MooEncoderTests.swift
@@ -17,7 +17,7 @@ final class MooEncoderTests: XCTestCase {
         let messages: [MooMessage] = [
             MooMessage(requestID: 1, verb: SwiftRoonAPI.MooVerb.complete, name: .success),
             MooMessage(requestID: 0, verb: SwiftRoonAPI.MooVerb.request, name: .info),
-            MooMessage(requestID: 1, verb: SwiftRoonAPI.MooVerb.request, name: .register, headers: [SwiftRoonAPI.MooHeaderName.contentType: "application/json"], body: try! RoonExtensionRegInfo.makeEncoded()),
+            MooMessage(requestID: 1, verb: SwiftRoonAPI.MooVerb.request, name: .register, headers: [SwiftRoonAPI.MooHeaderName.contentType: .applicationJson], body: try! RoonExtensionRegInfo.makeEncoded()),
             MooMessage(requestID: 2, verb: SwiftRoonAPI.MooVerb.request, name: .transport + "/subscribe_" + .zones, body: try! SubscriptionBody.makeEncoded()),
             MooMessage(requestID: 4, verb: SwiftRoonAPI.MooVerb.request, name: .control, body: try! ZoneControl.makeEncoded(.playpause)),
             MooMessage(requestID: 6, verb: SwiftRoonAPI.MooVerb.request, name: .control, body: try! ZoneControl.makeEncoded(.next)),
